1a6753671563666b65b8730afc86dacbbba74461
Fontelesxx <enzo.gabriel0@outlook.com>
2025-11-23 14:43:42 -0300

    Teste WhatsApp

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
   AOS.init(); // framework
 
+
 window.addEventListener('scroll', () => {
     const header = document.getElementById('barra_navegacao');
     if (window.scrollY > 40) header.classList.add('scrolled');
     else header.classList.remove('scrolled');
 })// Animação nav
+
+
 
 
