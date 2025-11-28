e9e0c913f5f37bb07f3a23ad0cfe559741df37b7
Fontelesxx <enzo.gabriel0@outlook.com>
2025-11-28 20:08:49 -0300

    Agendamento v1

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-  AOS.init(); // framework
+AOS.init(); // framework
 
 
-  
+
 
 window.addEventListener('scroll', () => {
-    const header = document.getElementById('barra_navegacao');
-    if (window.scrollY > 40) header.classList.add('scrolled');
-    else header.classList.remove('scrolled');
+  const header = document.getElementById('barra_navegacao');
+  if(!header)return;
+  if (window.scrollY > 40) header.classList.add('scrolled');
+  else header.classList.remove('scrolled');
 })// Animação nav
-
 
 
