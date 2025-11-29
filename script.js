960aa5eca73b7dca9aedd02332f063a24e00e597
Fontelesxx <enzo.gabriel0@outlook.com>
2025-11-28 23:10:52 -0300

    Feedbacks

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ window.addEventListener('scroll', () => {
 })// Animação nav
 
 
+// Carrosel dos feedbacks
+        let index = 0;
+        const items = document.querySelectorAll(".feedback-item");
+        const dots = document.querySelectorAll(".dot");
+
+        function showSlide(i) {
+            items.forEach(item => item.classList.remove("active"));
+            dots.forEach(dot => dot.classList.remove("active"));
+
+            items[i].classList.add("active");
+            dots[i].classList.add("active");
+        }
+
+        function nextSlide() {
+            index = (index + 1) % items.length;
+            showSlide(index);
+        }
+
+        showSlide(index);
+        setInterval(nextSlide, 5000); // troca a cada 5 segundos
